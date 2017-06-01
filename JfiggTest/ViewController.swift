a3273d7b0203d3e684e280dcdef9f2a1e1339756
bfleury2017 <bfleury@outlook.com>
2017-06-01 16:48:37 -0400

    Initial

--- JfiggTest/ViewController.swift
+++ JfiggTest/ViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    var TipPercent:Double = 0
+    
+    @IBOutlet weak var TipVal: UILabel!
+    @IBOutlet weak var TotalVal: UILabel!
+    @IBAction func PercentVal(_ sender: Any) {
+        CalculateTip()
+    }
+    @IBOutlet weak var PercentOut: UISegmentedControl!
+    @IBOutlet weak var AmountOut: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        AmountOut.addTarget(self, action: #selector(TextFieldDidChange), for: .editingChanged)
+        PercentOut.selectedSegmentIndex = 1
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +31,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    return true
+    }
+    
+    func TextFieldDidChange() {
+        CalculateTip()
+    }
+    func CalculateTip(){
+        switch(PercentOut.selectedSegmentIndex){
+        case 0: TipPercent = 10
+        case 1: TipPercent = 20
+        case 2: TipPercent = 30
+        default: TipPercent = 10
+        }
+        let BillDefault = AmountOut.text?.characters.count
+        if (BillDefault! > 0){
+            let Numbers = Double(AmountOut.text!)
+            let Tip = Numbers! * TipPercent / 100
+            let Total = Numbers! + Tip
+            let Formater = NumberFormatter()
+            Formater.numberStyle = .currency
+            Formater.locale = Locale(identifier: "en-US")
+            TipVal.text = Formater.string(from: Tip as NSNumber)
+            TotalVal.text = Formater.string(from: Total as NSNumber)
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        AmountOut.resignFirstResponder()
+        super.touchesBegan(touches, with: event)
+    }
 }
 
